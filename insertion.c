17f9c5e98c2191697ea70ecb73376abcd63a0e19
El Mehdi El Brahmi <eel-brah@e3r8p16.1337.ma>
2023-12-17 18:41:37 +0100

    Update

---
+++ insertion.c
@@ -0,0 +1,52 @@
+#include "push_swap.h"
+
+
+void sheft_put(int i, int j, int *array)
+{
+	int t;
+	int	k;
+
+	if (j < 0 || j == i)
+		return ;
+	t = array[i];
+	while (j < i)
+	{
+		array[i] = array[i - 1];
+		i--;
+	}
+	array[j] = t;
+}
+#include <limits.h>
+
+void insertion(int *array, int arr_size)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while(i < arr_size)
+	{
+		j = i - 1;
+		while (j >= 0 && array[j] > array[i])
+			j--;
+		sheft_put(i, j + 1, array);
+		i++;
+	}
+}
+
+int main()
+{
+	int i = 0;
+	int j = 0;
+	int array[] = {-1, INT_MAX, INT_MIN , 9, 1, 55, 'a', 'i', };
+	int arr_size = sizeof(array) / sizeof(int);
+
+	while (i < arr_size)
+		printf("%i, ", array[i++]);
+	printf("\n---\n");
+	insertion(array, arr_size);
+	i = 0;
+	while (i < arr_size)
+		printf("%i, ", array[i++]);
+	printf("\n");
+}
--- merge.c
+++ merge.c
@@ -1,73 +1,88 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int  *merge(int *ptr , int size)
+void fu()
 {
-	int size1;
-	int size2;
+	system("leaks mer");
+}
+
+int	*combine(int *p1, int *p2, int size, int size_1, int size_2)
+{
+	int *tmp_array;
 	int i;
 	int j;
-	int *p1;
-	int *p2;
-	if (size <= 1)
-		return (ptr);
-	size1 = size - size / 2;
-	size2 = size / 2;
-	p1 = merge(ptr, size - size2);
-	p2 = merge(ptr + size1, size - size1);
-	int array[size];
+	int k;
+
+	tmp_array = malloc(sizeof(int) * size);
 	i = 0;
 	j = 0;
-	int k = 0;
-	while (j < size / 2 && i < size - size / 2 )
+	k = 0;
+	while (j < size_2 && i < size_1)
 	{
 		if (p1[i] < p2[j])
-		{
-			array[k] = p1[i];
-			i++;
-			k++;
-		}
+			tmp_array[k] = p1[i++];
 		else
-		{
-			array[k] = p2[j];
-			k++;
-			j++;
-		}
+			tmp_array[k] = p2[j++];
+		k++;
 	}
 	if (j < i)
 	{
-		while (j < size / 2)
-		{
-			array[k++] = p2[j++];
-		}
+		while (j < size_2)
+			tmp_array[k++] = p2[j++];
 	}
 	else
 	{
-		while (i < size - size / 2)
-		{
-			array[k++] = p1[i++];
-		}
+		while (i < size_1)
+			tmp_array[k++] = p1[i++];
 	}
+	return (tmp_array);
+}
+
+int  *merge_sort(int *ptr , int size)
+{
+	//atexit(fu);
+	int size_1;
+	int size_2;
+	int *p1;
+	int *p2;
+	int *tmp_array;
+
+	int i;
+	int j;
+	int k;
+
+	if (size <= 1)
+		return (ptr);
+	size_1 = size - size / 2;
+	size_2 = size / 2;
+	p1 = merge_sort(ptr, size_1);
+	p2 = merge_sort(ptr + size_1, size_2);
+
+	tmp_array = combine(p1, p2, size, size_1, size_2);
+	//merge(tmp_array, ptr, size);
 	int x = 0;
 	while (x < size)
 	{
-		ptr[x] = array[x];
+		ptr[x] = tmp_array[x];
 		x++;
 	}
+	free(tmp_array);
 	return ptr;
 }
 
+#include <limits.h>
 int main()
 {
-	int array[] = {7, 5, 4, 9, 1, 55, 88, 909, 0, 888, 22222};
+	int array[] = {-1, INT_MAX, INT_MIN , 9, 1, 55, 'a', 'i', 0, 888, 22222};
+
 	int size = sizeof(array) / sizeof(int);
 	int i = 0;
 	while(i < size)
-		printf("%i - ", array[i++]);
+		printf("%i, ", array[i++]);
 	printf("\n");
-	merge(array, size);
+	merge_sort(array, size);
 	i = 0;
 	while(i < size)
-		printf("%i - ", array[i++]);
+		printf("%i, ", array[i++]);
 	printf("\n");
 }
